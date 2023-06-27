ce6d1b867990a4ffa99aa2a226af357174a6292d
Allan <contato@allancruz.com.br>
2023-06-27 12:14:35 -0300

    feat: return characters

--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,17 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import api from "../../services/api"
+
+interface propCharacters {
+  name: string,
+  description: string
+}
 
 export default function Characters() {
 
+  const [characters, setCaracters] = useState<propCharacters[]>([])
+
   useEffect(() => {
     api.get('/characters')
-    .then(response => console.log(response.data.data))
+    .then(response => {
+      setCaracters(response.data.data.results)
+    })
     .catch(err => console.log('erro', err))
   }, [] )
     
   return(
       <div>
-        Herois
+        { characters.map( (item) =>
+          <div>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+          </div> 
+        ) }
       </div>
   )
 }
